53a32fb95323f2f29384e90ebf0d931ec40aaf08
Juan Carlos Zegarra Ayllon <juancarloszegarraayllon@Juans-MacBook-Pro-4.local>
2018-08-18 14:54:26 -0400

    Node-Express-Handlebars

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+//CREATE THE MySQL CONNECTION AND EXPORT FOR THE DATABASE BY THE ORM 
+
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+ 	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "hacktheplanet",
+		database: "todoagain_db"
+	});
+};
+
+connection.connect(function(err) {
+    if (err) {
+    	console.error('error connecting: ' + err.stack);
+    	return;
+    } 
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports= connection;
